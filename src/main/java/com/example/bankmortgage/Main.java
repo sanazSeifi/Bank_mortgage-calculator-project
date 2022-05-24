577ad8dd898e78d7c1873213a1b1be1310d3a3c0
Sanaz <snz5seifi@gmail.com>
2022-05-24 17:20:57 -0400

    Refactoring by extract Method

--- src/main/java/com/example/bankmortgage/Main.java
+++ src/main/java/com/example/bankmortgage/Main.java
@@ -14,20 +14,27 @@ public class Main{
         byte years = (byte) readNumber("Period (Years) :" ,1,30);
 
 
+        PrintMortgage(principle, annualInterest, years);
 
-        double mortgage= calculateMortgage(principle,annualInterest,years);
+        PrintPaymentSchedule(principle, annualInterest, years);
+    }
+
+    private static void PrintMortgage(int principle, float annualInterest, byte years) {
+        double mortgage= calculateMortgage(principle, annualInterest, years);
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println();
         System.out.println("Mortgage");
         System.out.println("-------");
         System.out.println("Monthly Payments " + mortgageFormatted);
         System.out.print("Mortgage :" + mortgageFormatted);
+    }
 
+    private static void PrintPaymentSchedule(int principle, float annualInterest, byte years) {
         System.out.println();
         System.out.println("Payment Schedule");
         System.out.println("-------");
 
-        for( short month =1; month< years* MONTH_IN_YEAR; month++) {
+        for(short month = 1; month< years * MONTH_IN_YEAR; month++) {
             double balance = calculateBalance(principle, annualInterest, years, month);
             System.out.println(NumberFormat.getCurrencyInstance().format(balance));
         }
