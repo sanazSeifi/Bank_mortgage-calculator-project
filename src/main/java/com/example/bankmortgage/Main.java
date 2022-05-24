1068f562577cd43929be52f1f7797e767d4ea68b
Sanaz <snz5seifi@gmail.com>
2022-05-24 12:27:57 -0400

    refactoring code with putting value beside of all numbers

--- src/main/java/com/example/bankmortgage/Main.java
+++ src/main/java/com/example/bankmortgage/Main.java
@@ -7,18 +7,9 @@ public class Main{
 
     public static void main(String[] args) {
 
-        final byte MONTH_IN_YEAR = 12;
-        final byte PERCENT = 100;
-        int principle;
-        float annualInterest = 0;
-        byte years=0;
-        float  numberOfPayment = years * MONTH_IN_YEAR;
-        float monthlyInterest = annualInterest / PERCENT / MONTH_IN_YEAR;
-
-
-        principle = (int) readNumber("Principle: ", 1000, 1_000_000);
-        annualInterest = (float) readNumber("Annual Interest Rate: ", 1 , 30);
-        years = (byte) readNumber("Period (Years) :" ,1,30);
+        int principle = (int) readNumber("Principle: ", 1000, 1_000_000);
+        float annualInterest = (float) readNumber("Annual Interest Rate: ", 1 , 30);
+        byte years = (byte) readNumber("Period (Years) :" ,1,30);
 
 
 
@@ -51,8 +42,9 @@ public class Main{
             if (value >= min && value <= max) {
                 break;
             }
-            System.out.println("Enter Value Between" + min + "and" + max);
+            System.out.println("Enter Value Between " + min + " and " + max);
 
         }
+        return value;
     }
 }
